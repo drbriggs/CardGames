5362f1f6ea2d5604143d3b66d239cb1bdbc8803d
Dan <Malakili27@hotmail.com>
2013-07-30 12:48:06 -0700

    Create __init__.py
    starting __init__.py for packaging needs.  Still needs a lot of questions answered.

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from deck import Deck
+from wargameengine import *
+
+#What all needs to be imported here?
+#Can we just do top level and each file will still run its own imports?
+#what's the best way to import wargameengine?  There are several functions inside.
