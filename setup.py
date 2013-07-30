a590aedd082472b0ea9551ad67dd60c0939d2f9d
ccozad <ccozad@gmail.com>
2013-07-30 12:50:51 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cardgames',
+    version='1.0',
+    description='Models and behavior used to play and simulate card games',
+    url='http://github.com/Malakili27/CardGames',
+    author='Daniel R. Briggs',
+    author_email='anonymous@example.com',
+    license='MIT',
+    packages=['cardgames'],
+    zip_safe=False)
